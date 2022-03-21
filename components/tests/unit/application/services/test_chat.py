25520c2960bad9201cbf34a053d3a0f327efc59a
Alex Vershinin <72911805+Kanroshka@users.noreply.github.com>
2022-03-21 16:52:03 +0500

    Update test_chat.py

--- components/tests/unit/application/services/test_chat.py
+++ components/tests/unit/application/services/test_chat.py
@@ -52,22 +52,3 @@ def test_add_user(service_user):
     service_user.add_user(**data_user)
     service_user.user_repo.add.assert_called_once()
 
-
-# def test_get_info_chat(service_chat):
-#     chat = service_chat.data_message(id=1)
-#     assert asdict(chat) == data_chat
-#
-#
-# def test_add_chat_user(service_chat):
-#     service_chat.get_by_chat_id(**data_chat_user)
-#     service_chat.chat_members_repo.add.assert_called_once()
-#
-#
-# def test_get_chat(service_chat):
-#     chat = service_chat.get_chat_by_id(chat_id=1)
-#     assert asdict(chat) == data_chat
-#
-#
-# def test_get_user(service_chat):
-#     user = service_chat.get_users(user_id=data_user['id_user'])
-#     assert asdict(user) == data_user
